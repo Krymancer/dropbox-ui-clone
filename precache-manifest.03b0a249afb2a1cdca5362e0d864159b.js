593ca07fc668bbd09a6fc05da6a83dde0f25cf8b
krymancer <junior.nascm@gmail.com>
2020-09-01 23:03:34 -0300

    Updates

---
+++ precache-manifest.03b0a249afb2a1cdca5362e0d864159b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c41426415e097401a81104cf8d2c2b4",
+    "url": "/dropbox-ui-clone/index.html"
+  },
+  {
+    "revision": "2d0053dbb15b1f9e4fb3",
+    "url": "/dropbox-ui-clone/static/js/2.8950b6fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dropbox-ui-clone/static/js/2.8950b6fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d67ff3aa2c2f94e6ed",
+    "url": "/dropbox-ui-clone/static/js/main.9ac2fdec.chunk.js"
+  },
+  {
+    "revision": "a874e1d94092336c4126",
+    "url": "/dropbox-ui-clone/static/js/runtime-main.8e7474a5.js"
+  }
+]);
